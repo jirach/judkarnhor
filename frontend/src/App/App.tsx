fdd93d9d32888ecdc428c32b63cb1c83db1a478b
Jirach Suthammanas <jirach@gmail.com>
2021-04-03 15:31:59 +0700

    eslint enabled

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router, Link, Route, Switch,
+} from 'react-router-dom';
 import Dashboard from '../Pages/Dashboard/Dashboard';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
@@ -7,14 +9,18 @@ import Login from '../Pages/Login/Login';
 const App: React.FC = () => (
   <div>
     <Router>
-    <Link to="/">Home</Link> <Link to="/login">Login</Link> <Link to="/dashboard">Dashboard</Link>
+      <Link to="/">Home</Link>
+      {' '}
+      <Link to="/login">Login</Link>
+      {' '}
+      <Link to="/dashboard">Dashboard</Link>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   </div>
-)
+);
 
 export default App;

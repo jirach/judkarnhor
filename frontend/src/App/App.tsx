cdf7206c9160754124cd4d358814f52065990cb6
Jirach Suthammanas <jirach@gmail.com>
2021-04-03 15:08:46 +0700

    add router

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,17 +1,21 @@
-import { Box, Container, Typography } from '@material-ui/core';
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import Dashboard from '../Pages/Dashboard/Dashboard';
+import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
 
-function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-      </Box>
-    </Container>
-  );
-}
+const App: React.FC = () => (
+  <div>
+    
+    <Router>
+    <Link to="/">Home</Link> <Link to="/login">Login</Link> <Link to="/dashboard">Dashboard</Link>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/dashboard' component={Dashboard} />
+      </Switch>
+    </Router>
+  </div>
+)
 
 export default App;
---
+++ frontend/src/Pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+
+  return (
+    <div>
+      Dashboard
+    </div>
+  )
+}
+
+export default Dashboard;
